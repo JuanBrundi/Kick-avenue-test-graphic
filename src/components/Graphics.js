bd96efcd641b8640a04c447786db915717516d83
juanbrundism <juanbrundism@gmail.com>
2020-11-11 21:30:43 +0700

    Using React-chart2-JS no X-Axes Range

--- src/components/Graphics.js
+++ src/components/Graphics.js
@@ -1,3 +1,254 @@
+// import React, { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Line } from "react-chartjs-2";
+
+// import sellingDataAction from "../store/actions/sellingDataAction";
+
+// function Graphics() {
+//   const dispatch = useDispatch();
+//   const { sellingData } = useSelector((state) => state.sellingDataReducer);
+//   const kamusBulan = {
+//     "01": "January",
+//     "02": "February",
+//     "03": "March",
+//     "04": "April",
+//     "05": "Mei",
+//     "06": "June",
+//     "07": "July",
+//     "08": "August",
+//     "09": "Septempber",
+//     "10": "October",
+//     "11": "November",
+//     "12": "December",
+//   };
+
+//   const [data, setData] = useState();
+//   const [labels, setLabels] = useState();
+//   const [dataSetData, setDataSetData] = useState();
+
+//   useEffect(() => {
+//     dispatch(sellingDataAction());
+//   }, [dispatch]);
+
+//   function setDataForGraph() {
+
+//       // Sort by higher price
+//       let sellData = sellingData?.data?.[6];
+//       let tempObj = {};
+//       sellData?.forEach((shoe) => {
+//         const year = shoe.customer_paid.split(" ")[0].split("-")[0];
+//         const month = shoe.customer_paid.split(" ")[0].split("-")[1];
+//         const date = shoe.customer_paid.split(" ")[0].split("-")[2];
+//         const fullDate = shoe.customer_paid;
+//         const price = shoe.price;
+
+//         if (tempObj[`${date}${month}${year}`] === undefined) {
+//           tempObj[`${date}${month}${year}`] = [
+//             `${date} ${kamusBulan[month]} ${year}`,
+//             price,
+//             year,
+//             fullDate
+//           ];
+//         } else {
+//           if (tempObj[`${date}${month}${year}`].price < price) {
+//             tempObj[`${month + date}`].price = price;
+//           }
+//         }
+//       });
+
+//       // Sort by Date
+//       let tempObjValue = Object?.values(tempObj)
+//       var length = tempObjValue?.length;
+//       for (var i = 0; i < length; i++) {
+//         for (var j = 0; j < length - i - 1; j++) {
+//           if (tempObjValue[j]?.[3] > tempObjValue[j + 1]?.[3]) {
+//             var tmp = tempObjValue[j];
+//             tempObjValue[j] = tempObjValue[j + 1];
+//             tempObjValue[j + 1] = tmp;
+//           }
+//         }
+//       }
+
+//       // Get labels and dataset.data for Chart
+//       let allLabels = [];
+//       let allDataSetsData = [];
+//       tempObjValue.forEach(value => {
+//         allLabels.push(value[0]);
+//         allDataSetsData.push(value[1]);
+//       })
+
+      
+//       const tempData = {
+//         labels: allLabels,
+//         datasets: [
+//           {
+//             label: "Shoes Sale History",
+//             fill: true,
+//             lineTension: 0.3,
+//             // backgroundColor: "linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1))",
+//             backgroundColor: "rgba(75,192,192,0.4)",
+//             borderColor: "rgba(75,192,192,1)",
+//             borderCapStyle: "butt",
+//             borderDash: [],
+//             borderDashOffset: 0.0,
+//             borderJoinStyle: "miter",
+//             pointBorderColor: "rgba(75,192,192,1)",
+//             pointBackgroundColor: "#fff",
+//             pointBorderWidth: 1,
+//             pointHoverRadius: 5,
+//             pointHoverBackgroundColor: "rgba(75,192,192,1)",
+//             pointHoverBorderColor: "rgba(220,220,220,1)",
+//             pointHoverBorderWidth: 2,
+//             pointRadius: 5,
+//             pointHitRadius: 10,
+//             data: allDataSetsData,
+//           },
+//         ],
+//       };
+//       // setLabels(labels);
+//       // setDataSetData(dataSetData);
+//       // setData(tempData);
+//       console.log(tempData)
+//       return tempData
+//     }
+
+//   useEffect(() => {
+//     // function setDataForGraph() {
+
+//     //   // Sort by higher price
+//     //   let sellData = sellingData?.data?.[6];
+//     //   let tempObj = {};
+//     //   sellData?.forEach((shoe) => {
+//     //     const year = shoe.customer_paid.split(" ")[0].split("-")[0];
+//     //     const month = shoe.customer_paid.split(" ")[0].split("-")[1];
+//     //     const date = shoe.customer_paid.split(" ")[0].split("-")[2];
+//     //     const fullDate = shoe.customer_paid;
+//     //     const price = shoe.price;
+
+//     //     if (tempObj[`${date}${month}${year}`] === undefined) {
+//     //       tempObj[`${date}${month}${year}`] = [
+//     //         `${date} ${kamusBulan[month]} ${year}`,
+//     //         price,
+//     //         year,
+//     //         fullDate
+//     //       ];
+//     //     } else {
+//     //       if (tempObj[`${date}${month}${year}`].price < price) {
+//     //         tempObj[`${month + date}`].price = price;
+//     //       }
+//     //     }
+//     //   });
+
+//     //   // Sort by Date
+//     //   let tempObjValue = Object?.values(tempObj)
+//     //   var length = tempObjValue?.length;
+//     //   for (var i = 0; i < length; i++) {
+//     //     for (var j = 0; j < length - i - 1; j++) {
+//     //       if (tempObjValue[j]?.[3] > tempObjValue[j + 1]?.[3]) {
+//     //         var tmp = tempObjValue[j];
+//     //         tempObjValue[j] = tempObjValue[j + 1];
+//     //         tempObjValue[j + 1] = tmp;
+//     //       }
+//     //     }
+//     //   }
+
+//     //   // Get labels and dataset.data for Chart
+//     //   let allLabels = [];
+//     //   let allDataSetsData = [];
+//     //   tempObjValue.forEach(value => {
+//     //     allLabels.push(value[0]);
+//     //     allDataSetsData.push(value[1]);
+//     //   })
+
+      
+//     //   const tempData = {
+//     //     labels: allLabels,
+//     //     datasets: [
+//     //       {
+//     //         label: "Shoes Sale History",
+//     //         fill: true,
+//     //         lineTension: 0.3,
+//     //         backgroundColor: "linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1))",
+//     //         // backgroundColor: "rgba(75,192,192,0.4)",
+//     //         borderColor: "rgba(75,192,192,1)",
+//     //         borderCapStyle: "butt",
+//     //         borderDash: [],
+//     //         borderDashOffset: 0.0,
+//     //         borderJoinStyle: "miter",
+//     //         pointBorderColor: "rgba(75,192,192,1)",
+//     //         pointBackgroundColor: "#fff",
+//     //         pointBorderWidth: 1,
+//     //         pointHoverRadius: 5,
+//     //         pointHoverBackgroundColor: "rgba(75,192,192,1)",
+//     //         pointHoverBorderColor: "rgba(220,220,220,1)",
+//     //         pointHoverBorderWidth: 2,
+//     //         pointRadius: 5,
+//     //         pointHitRadius: 10,
+//     //         data: allDataSetsData,
+//     //       },
+//     //     ],
+//     //   };
+//     //   setLabels(labels);
+//     //   setDataSetData(dataSetData);
+//     //   setData(tempData);
+//     // }
+//     setDataForGraph();
+//   }, [sellingData]);
+  
+//   const getChartData = canvas => {
+//     const ctx = canvas?.getContext("2d")
+//     // const gradient = ctx?.createRadialGradient(0, 0, 600, 550)
+//     // gradient?.addColorStop(0, "rgba(75,192,192,0.4)")
+//     // gradient?.addColorStop(0, "rgba(133,122,144,0.5)")
+
+//     const tempData = {
+//       labels: labels,
+//       datasets: [
+//         {
+//           label: "Shoes Sale History",
+//           fill: true,
+//           lineTension: 0.3,
+//           backgroundColor: "rgba(75,192,192,0.4)",
+//           // backgroundColor: gradient,
+//           borderColor: "rgba(75,192,192,1)",
+//           borderCapStyle: "butt",
+//           borderDash: [],
+//           borderDashOffset: 0.0,
+//           borderJoinStyle: "miter",
+//           pointBorderColor: "rgba(75,192,192,1)",
+//           pointBackgroundColor: "#fff",
+//           pointBorderWidth: 1,
+//           pointHoverRadius: 5,
+//           pointHoverBackgroundColor: "rgba(75,192,192,1)",
+//           pointHoverBorderColor: "rgba(220,220,220,1)",
+//           pointHoverBorderWidth: 2,
+//           pointRadius: 5,
+//           pointHitRadius: 10,
+//           data: dataSetData,
+//         },
+//       ],
+//     };
+//     console.log(tempData)
+//     return tempData
+//   }
+
+//   // useEffect(() => {
+//   //   getChartData()
+//   // }, [labels, dataSetData])
+//   // console.log(getChartData)
+//   return (
+//     <div>
+//       <h1>Kick Avenue</h1>
+//       <h3>Sale History Chart</h3>
+//       {data ? <Line data={setDataForGraph} /> : <h1>Loading...</h1>}
+//     </div>
+//   );
+// }
+
+// export default Graphics;
+
+
+
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Line } from "react-chartjs-2";
@@ -8,76 +259,89 @@ function Graphics() {
   const dispatch = useDispatch();
   const { sellingData } = useSelector((state) => state.sellingDataReducer);
   const kamusBulan = {
-    "01": "jan",
-    "02": "feb",
-    "03": "mar",
-    "04": "apr",
-    "05": "mei",
-    "06": "jun",
-    "07": "jul",
-    "08": "ags",
-    "09": "sep",
-    "10": "okt",
-    "11": "nov",
-    "12": "des",
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04": "April",
+    "05": "Mei",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "Septempber",
+    "10": "October",
+    "11": "November",
+    "12": "December",
   };
-  
-  const [data, setData] = useState();
+
+  const [loading, setLoading] = useState(false) ;
 
   useEffect(() => {
     dispatch(sellingDataAction());
+    setLoading(true)
   }, [dispatch]);
 
-  useEffect(() => {
-    function setDataForGraph() {
+  function getGradient(canvas) {
+    const ctx = canvas?.getContext("2d")
+    const gradient = ctx?.createLinearGradient(0, 0, 600, 550)
+    gradient?.addColorStop(0.1, "rgba(247, 202, 24, 1)")
+    gradient?.addColorStop(0.95, 'rgba(75,192,192,0.4)')
+    return gradient;
+  }
 
-      // SORT DATA BY DATE
-      let sorted = sellingData?.data
-      var length = sorted?.length;
-      for (var i = 0; i < length; i++) {
-        for (var j = 0; j < length - i - 1; j++) {
-          if (sorted[j]?.customer_paid > sorted[j + 1]?.customer_paid) {
-            var tmp = sorted[j]; 
-            sorted[j] = sorted[j + 1]; 
-            sorted[j + 1] = tmp; 
-          }
-        }
-      }
-
+  function setDataForGraph(canvas) {
+      // Sort by higher price
+      let sellData = sellingData?.data?.[6];
       let tempObj = {};
-      sorted?.forEach((shoe) => {
-        let year = shoe.customer_paid.split(" ")[0].split("-")[0];
-        let month = shoe.customer_paid.split(" ")[0].split("-")[1];
+      sellData?.forEach((shoe) => {
+        const year = shoe.customer_paid.split(" ")[0].split("-")[0];
+        const month = shoe.customer_paid.split(" ")[0].split("-")[1];
         const date = shoe.customer_paid.split(" ")[0].split("-")[2];
+        const fullDate = shoe.customer_paid;
         const price = shoe.price;
-        if (!tempObj[`${month + date}`]) {
-          tempObj[`${month + date}`] = [
-            `${date}.${kamusBulan[month]}`,
+
+        if (tempObj[`${date}${month}${year}`] === undefined) {
+          tempObj[`${date}${month}${year}`] = [
+            `${date} ${kamusBulan[month]} ${year}`,
             price,
             year,
+            fullDate
           ];
         } else {
-          if (tempObj[`${month + date}`].price <= price) {
+          if (tempObj[`${date}${month}${year}`].price < price) {
             tempObj[`${month + date}`].price = price;
           }
         }
       });
 
-      let allLabels = [];
-      let allDataSetsData = [];
-      for (const temp in tempObj) {
-        allLabels.push(tempObj[temp][0]);
-        allDataSetsData.push(+tempObj[temp][1]);
+      // Sort by Date
+      let tempObjValue = Object?.values(tempObj)
+      var length = tempObjValue?.length;
+      for (var i = 0; i < length; i++) {
+        for (var j = 0; j < length - i - 1; j++) {
+          if (tempObjValue[j]?.[3] > tempObjValue[j + 1]?.[3]) {
+            var tmp = tempObjValue[j];
+            tempObjValue[j] = tempObjValue[j + 1];
+            tempObjValue[j + 1] = tmp;
+          }
+        }
       }
 
+      // Get labels and dataset.data for Chart
+      let allLabels = [];
+      let allDataSetsData = [];
+      tempObjValue.forEach(value => {
+        allLabels.push(value[0]);
+        allDataSetsData.push(value[1]);
+      })
+      
       const tempData = {
         labels: allLabels,
         datasets: [
           {
-            label: "StockX Graphic",
-            fill: false,
+            label: "Shoes Sale History",
+            fill: true,
             lineTension: 0.3,
-            backgroundColor: "rgba(75,192,192,0.4)",
+            backgroundColor: getGradient(canvas),
             borderColor: "rgba(75,192,192,1)",
             borderCapStyle: "butt",
             borderDash: [],
@@ -96,17 +360,19 @@ function Graphics() {
           },
         ],
       };
-      setData(tempData);
+      setLoading(true)
+      return tempData
     }
-    setDataForGraph();
-  }, [sellingData]);
+
+    useDispatch(() => {
+      setDataForGraph()
+    }, [sellingData])
 
   return (
     <div>
-      <h1>Table</h1>
-      {
-        data ? <Line data={data} /> : <h1>Loading...</h1>
-      }
+      <h1>Kick Avenue</h1>
+      <h3>Sale History Chart</h3>
+      {loading ? <Line data={setDataForGraph} /> : <h1>Loading...</h1>}
     </div>
   );
 }
